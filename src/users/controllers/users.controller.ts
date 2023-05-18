59fd6c48d2aacbc2f07e0bc0e81d68d675ff0a25
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-05-18 17:47:45 +0100

    chore(): user post requests

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from '../services/users.service';
+import { User } from '../models/user.interface';
+import {Observable} from 'rxjs'
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private userService: UsersService) {}
+  
+    @Post()
+    create(@ Body() post: User): Observable<User>{
+        return this.userService.createPost(post)
+    }
+}
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { UserEntity } from '../models/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../models/user.interface';
+import { Observable , from} from 'rxjs';
 
 @Injectable()
 export class UsersService {
@@ -9,4 +11,8 @@ export class UsersService {
         @InjectRepository(UserEntity)
         private readonly userRepository: Repository<UserEntity>
     ){}
+
+    createPost(userPost: User): Observable<User> {
+        return from(this.userRepository.save(userPost))
+    }
 }
